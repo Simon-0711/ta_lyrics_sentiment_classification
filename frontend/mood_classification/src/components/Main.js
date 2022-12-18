1e4257b9c957aa9cdbefb971be171b3a70cb3fa2
Simon-0711 <Simon.pavicic99@gmail.com>
2022-12-18 16:51:03 +0100

    Add Artist Name input box

--- frontend/mood_classification/src/components/Main.js
+++ frontend/mood_classification/src/components/Main.js
@@ -5,8 +5,10 @@ export default function Main() {
         <div id='mainDiv'>
             <h1 id='mainHeader'>Looking for similar Songs?</h1>
             <p id='lyricsRequest'>Just give us a song and artist name. We will find songs with a similar mood while you sit back and relax. </p>
-            <div class="songNameInput">
-                <input type="text" name="inputLyrics" id="inputLyrics" required spellcheck="false" placeholder='Song Name'></input>
+
+            <div class="searchInput">
+                <input type="text" name="inputSong" id="inputSong" required spellcheck="false" placeholder='Song Name'></input>
+                <input type="text" name="inputArtist" id="inputArtist" required spellcheck="false" placeholder='Artist Name'></input>
             </div>
             <button id='searchSimilarLyricsButton' name="searchLyrics">Find Similar Songs</button>
         </div >
