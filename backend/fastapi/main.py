757ac30260fecf0917eb6c7d9cc044643bfc4099
Max Ludwig <maxludwig3@t-online.de>
2022-12-21 09:35:47 +0100

    deature: Add dummy endpoint to receive data for artists

--- backend/fastapi/main.py
+++ backend/fastapi/main.py
@@ -1,7 +1,21 @@
 from configuration import config
 from configuration.config import app as app
+from pydantic import BaseModel
 
 
 @app.get("/dummy-endpoint")
 async def root():
     return {"message": "Hello from the fastAPI backend!"}
+
+
+class Artist(BaseModel):
+    name: str
+
+class Song(BaseModel):
+    name: str
+
+
+@app.post("/search")
+async def search(Song: Song, Artist: Artist):
+    # search for song here 
+    return {"Song": Song, "Artist": Artist}
