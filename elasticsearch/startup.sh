8e0e40be4e01d019759359658b3f8d658798b73e
Max Ludwig <maxludwig3@t-online.de>
2023-02-03 11:15:35 +0100

    adapt to new script name and install elasticdump

--- elasticsearch/startup.sh
+++ elasticsearch/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 /bin/tini /usr/local/bin/docker-entrypoint.sh -- &
-/tmp/wait-for-it.sh -t 30 localhost:9200 -- python3 /tmp/create_es_index.py
+/tmp/wait-for-it.sh -t 30 localhost:9200 -- python3 /tmp/create_load_save_es_index.py
 while true; do sleep 100; done
